06af029fd4b26f49f0f0d51ab8b8122428e41496
masharzy <horsnbd@gmail.com>
2022-05-25 10:23:41 +0600

    Connected to MongoDB

--- index.js
+++ index.js
@@ -2,11 +2,52 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nsfvh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const run = async () => {
+  try {
+    await client.connect();
+    //tools collection
+    const toolsCollection = client.db("toptool").collection("tools");
+    
+    //get all tools
+    app.get("/tools", async (req, res) => {
+        const tools = await toolsCollection.find({}).toArray();
+        res.send(tools);
+    });
+
+    console.log("Connected to Database");
+  } finally {
+  }
+};
+
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
   res.send("TOPTOOL");
